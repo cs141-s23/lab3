f29e9e871289d79355ee1a1e535c7f689a5ad8cf
Dhilan Ramaprasad <38360278+dhilanr@users.noreply.github.com>
2023-03-08 20:58:22 -0500

    Update tlc to use `typedef enum` constructs

--- hdl/tlc.sv
+++ hdl/tlc.sv
@@ -11,27 +11,25 @@ module tlc
         output logic timer_load, timer_en
     );
 
-    logic [3:0] state, next_state;
-    logic sample_reg, sample_reg_next;
+    typedef enum {s_idle, s_two, s_three /*change these*/} state_t;
+    state_t state, next_state;
 
     // TODO: complete the implementation of the traffic light controller
 
     // synchronous logic (update the state on clock edge)
     always_ff @(posedge clk, posedge rst) begin
         if (rst) begin
-            state <= `IDLE;
-            sample_reg <= 0;
+            state <= s_idle;
         end
         else if (clk_en) begin
             state <= next_state;
-            sample_reg <= sample_reg_next;
         end
     end
 
     // combinational logic (determine what the next state is)
     always_comb begin
-        case (state)
-            `IDLE: begin
+        unique case (state)
+            s_idle: begin
                 // set outputs
                 light_ns = `LIGHT_RED;
                 light_ew = `LIGHT_RED;
@@ -43,12 +41,11 @@ module tlc
 
                 // sample state transition
                 if (ped)
-                    next_state = `SAMPLE_STATE1;
+                    next_state = s_two;
                 else
-                    next_state = `SAMPLE_STATE2;
+                    next_state = s_three;
 
                 // set next value for internal registers
-                sample_reg_next = 1;
             end
             default: begin
                 light_ns = `LIGHT_RED;
@@ -58,9 +55,8 @@ module tlc
                 timer_load = 1;
                 timer_init = 4'b1111;
 
-                next_state = `IDLE;
+                next_state = s_idle;
 
-                sample_reg_next = 0;
             end
         endcase
     end
